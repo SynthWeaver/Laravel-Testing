4567919684d52454e9ca3695bfe61dda53a328d1
Tim Terwijn <tim@tterwijn.nl>
2020-08-28 17:55:18 +0200

    database werkt

--- app/Http/Controllers/ShowHome.php
+++ app/Http/Controllers/ShowHome.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class ShowHome extends Controller
 {
@@ -13,6 +14,7 @@ class ShowHome extends Controller
      */
     public function __invoke()
     {
-        return view('home', ["phpWerkt" => "php werkt nu ook"]);
+        $user = DB::select('select * from user');
+        return view('home', ["phpWerkt" => "php werkt nu ook, en de database ook als hier tim staat:" . $user[0]->name]);
     }
 }
