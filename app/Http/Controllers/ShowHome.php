e2c59d5d87cff92fbdea844aac1745ab1998d223
Tim Terwijn <tim@tterwijn.nl>
2020-08-29 12:58:33 +0200

    added more database queries

--- app/Http/Controllers/ShowHome.php
+++ app/Http/Controllers/ShowHome.php
@@ -14,7 +14,29 @@ class ShowHome extends Controller
      */
     public function __invoke()
     {
+        $this->insert("mees");
+        $this->update("mees", "rik");
+        $this->delete("rik");
+
         $user = DB::select('select * from user');
         return view('home', ["phpWerkt" => "php werkt nu ook, en de database ook als hier tim staat:" . $user[0]->name]);
     }
+
+    private function insert($name){
+        DB::insert('insert into user (name) values (?)', [$name]);
+    }
+
+    private function select($name){
+        DB::insert('insert into user (name) values (?)', [$name]);
+    }
+
+    private function update($oldName, $newName){
+        DB::update('update user set name = ? where name = ?', [$newName, $oldName]);
+    }
+
+    private function delete($name){
+        DB::delete('delete from user where name = ?',[$name]);
+    }
+
+
 }
