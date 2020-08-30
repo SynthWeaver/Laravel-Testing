1b5a44c93280a4ddc1257aff502482bee44e8568
Tim Terwijn <tim@tterwijn.nl>
2020-08-30 14:30:32 +0200

    Added all things for my internship

--- resources/views/stage.blade.php
+++ resources/views/stage.blade.php
@@ -10,8 +10,283 @@
 </head>
 <body>
   <main>
-    <h1>Alle info over tims stage bij onestepsolutions</h1>
+    <h1>Alle info over Tim's stage bij Onestepsolutions</h1>
+
+    <article hidden>
+      <h2>Overzicht</h2>
+      <ul>
+        <li><a href="#planning">Planning</a></li>
+        <li><a href="#Taken">Taken</a></li>
+      </ul>
+    </article>
+
+    <article>
+      <h2>Termen</h2>
+      <ul>
+        <li>Stagebegeleider = Docent van school</li>
+        <li>Loopbaanadviseur = Pieter Leek</li>
+        <li>Bedrijfsbegeleider = Ron</li>
+      </ul>
+    </article>
     
+    <article id="taken">
+      <h2>Taken</h2>
+      <ul>
+        <li><a href="#" class="done">Beroepstaken</a></li>
+        <li><a href="#" class="done">Competent op gevorderd niveau</a></li>
+        <li><a href="#stageopdracht">Stageopdracht</a></li>
+        <li><a href="#onderzoeksrapport">Het onderzoeksrapport</a></li>
+        <li><a href="#portfolio">Het portfolio</a></li>
+        <li><a href="#stageassessment">Stage-assessment</a></li>
+        <li><a href="#stagepresentatie">Stagepresentatie</a></li>
+        <li><a href="#nabeschouwing">Nabeschouwing</a></li>
+      </ul>
+    </article>
+
+    <article id="stageopdracht">
+      <h2>Stageopdracht</h2>
+      <p>Heb ik het maandag met Ron over. Het is iets met hardlopen geloof ik.</p>
+    </article>
+
+    <article id="onderzoeksrapport">
+      <h2>Het onderzoeksrapport</h2>
+      <ul>
+        <li>Onderzoeks vraag (check competenties voor een goed onderzoek)</li>
+        <li>Ik dacht aan scrum toepassen bij Onestepsolutions</li>
+      </ul>
+    </article>
+
+    <article id="portfolio">
+      <h2>Portfolio</h2>
+      <h3>6 HBO Competenties</h3>
+      <ul>
+        <li>Professioneel vakmanschap</li>
+        <li>Onderzoekend vermogen</li>
+        <li>Leervermogen</li>
+        <li>Communicatief vermogen</li>
+        <li>Beroepsethiek en maatschappelijke oriëntatie (niet verplicht, mag ook buiten stage, wel met ICT, anders moet je een visie schrijven)</li>
+        <li>Samenwerken (niet verplicht, mag ook buiten stage, wel met ICT, anders moet je een visie schrijven)</li>
+      </ul>
+      <p>Per competentie vul je een competentieformulier in, een of meerdere star formulier en bewijs.</p>
+      <p>Een CV</p>
+      <p>Misschien is het slim om aan het einde van de week mijzelf te reflecteren en wat competenties opschrijven. Dan vergeet ik niks.</p>
+    </article>
+
+    <article id="stageassessment">
+      <h2>Stage assessment</h2>
+      <p>Het eind gesprek van 1,5 uur met je stagebegeleider en nog een docent</p>
+    </article>
+
+    <article id="stagepresentatie">
+      <h2>Stagepresentatie</h2>
+      <p>Een nutteloze presentatie op school.</p>
+      <ul>
+        <li>Gebruik powerpoint</li>
+        <li>Vertel iets korts over jezelf</li>
+        <li>Praat over je bedrijf</li>
+        <li>Praat over de opdracht waar je mee bezig bent. (VRAAG TOESTEMING RON!!!)</li>
+        <li>Praat over de gebruikte technieken</li>
+      </ul>
+    </article>
+
+    <article id="nabeschouwing">
+      <h2>Nabeschouwing</h2>
+      <p>Een nutteloos verslag, heb het over je keuzes en dingen die je wilt mee geven aan de volgende stagaires van de HvA.</p>
+    </article>
+
+    <article id="planning">
+      <h2>Planning</h2>
+      <h3>Legenda</h3>
+      <p>Groen = Feestdag</p>
+      <p>Rood = Snipperdag</p>
+      <p>? = ergens deze week</p>
+      <table>
+        <tr>
+          <td></td>
+          <td>Maandag</td>
+          <td>Dinsdag</td>
+          <td>Woensdag</td>
+          <td>Donderdag</td>
+          <td>Vrijdag</td>
+        </tr>
+        <tr>
+          <td> 31 augustus <br> Week 1</td>
+          <td>Eerste werkdag <br> Afspraak maken Stagebegeleider en Pieter <br> Bespreek uren met Ron <br> Afspraak maken met Ron tussentijds en eindgesprek</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>7 september <br> Week 2</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>14 september <br> Week 3</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>21 september <br> Week 4</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>28 september <br> Week 5</td>
+          <td>Stagebegeleider & Akkoord onderzoeksvraag?</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>5 october <br> Week 6</td>
+          <td>Pieter Leek Intervisie 1?</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>12 october <br> Week 7</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>19 october <br> week 8 </td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>26 october <br> Week 9</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>2 november <br> Week 10</td>
+          <td>Pieter Leek Intervisie 2? Tussentijdse beoordeling bedrijf?</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>9 november <br> Week 11</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>16 november <br> Week 12</td>
+          <td>Stagepresentatie?</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>23 november <br> Week 13</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>30 november <br> Week 14</td>
+          <td>Onderzoeksrapport & Beoordelingsformulier Inleveren?</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>7 december <br> Week 15</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>14 december <br> Week 16</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>21 december <br> Week 17</td>
+          <td>Portfolio af, inleveren Stagebegeleider?</td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+          <td class="vrij"></td>
+        </tr>
+        <tr>
+          <td>28 december <br> Week 18</td>
+          <td>Stagebegeleider eindgesprek? Portfolio inleveren online?<br> </td>
+          <td></td>
+          <td></td>
+          <td class="vrij"></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>4 januari <br> Week 19</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>11 januari <br> Week 20 </td>
+          <td>Nabeschouwing?</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Reflecteren</td>
+        </tr>
+        <tr>
+          <td>18 januari <br> Week 21</td>
+          <td>Stage-assessment?</td>
+          <td>Laatste werkdag</td>
+          <td class="thuis"></td>
+          <td class="thuis"></td>
+          <td class="thuis">Reflecteren</td>
+        </tr>
+        <tr>
+          <td>25 januari <br> Week 22</td>
+          <td class="thuis"></td>
+          <td class="thuis"></td>
+          <td class="thuis"></td>
+          <td class="thuis"></td>
+          <td class="thuis">Reflecteren</td>
+        </tr>
+      </table>
+    </article>
   </main>
   <script type="module" src="./js/stage.js"></script>
 </body>
